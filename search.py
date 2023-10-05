d995c2c770723c6767c6effc9cc7c8bb194be8dd
melanie-fressard <Melanie.Fressard@inspection.gc.ca>
2023-10-05 13:59:09 +0000

    can use search.py with args

--- search.py
+++ search.py
@@ -4,32 +4,54 @@
  
 
 #This is used to load the .env file
-
 import dotenv
 dotenv.load_dotenv()
-
 import louis.db as db
+from louis.models import openai
+import sys
 
-def search(cursor, query_embedding, text, JSONB):
+def search(cursor, query_embedding):
     """Match documents with a given query."""
     data = {
-        'text': text,
+        'text': "text",
         'query_embedding': query_embedding,
         'match_threshold': 0.5,
         'match_count': 10,
-        'weights': JSONB
+        'weights': "1"
     }
 
-    # cursor.callproc('search', data)
     cursor.execute(
-        "SELECT * FROM search",
-        "(%(text)s, %(query_embedding)s::vector, %(match_threshold)s, %(match_count)s, %(weights)s)",
+        "SELECT * FROM search"
+        "(%(text)s::text, %(query_embedding)s::vector, %(match_threshold)s, %(match_count)s::integer, %(weights)s::JSONB)",
         data)
     
     # turn into list of dict now to preserve dictionaries
     return [dict(r) for r in cursor.fetchall()]
 
+def search_from_text_query(cursor, query):
+    data = {
+        'query': query,
+        'tokens': openai.get_tokens_from_text(query)
+    }
+    results = cursor.execute("""
+        SELECT *
+        FROM query
+        WHERE tokens = %(tokens)s::integer[]
+    """, data)
+    db_data = results.fetchone()
+    if not db_data:
+        data['embedding'] = openai.fetch_embedding(data['tokens'])
+        results = cursor.execute(
+            "INSERT INTO query(query, tokens, embedding)"
+            " VALUES(%(query)s, %(tokens)s, %(embedding)s) RETURNING id", data)
+        data['query_id'] = results.fetchone()['id']
+    else:
+        data.update(db_data)
+    docs = search(cursor, data['embedding'])
+
+    return docs
+
 if __name__ == '__main__':
     connection = db.connect_db()
     with db.cursor(connection) as cursor:
-        search(cursor, 'what are the cooking temperatures for e.coli?')
+        search_from_text_query(cursor, ' '.join(sys.argv[1:]))
