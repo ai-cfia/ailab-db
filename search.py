9335248c665281d309257de76ca29931968f47b9
melanie-fressard <Melanie.Fressard@inspection.gc.ca>
2023-10-20 17:43:15 +0000

    issue #20 - ajout FunctionCall, ReturnValue

--- search.py
+++ search.py
@@ -6,16 +6,19 @@ import subprocess
 import sys
 import louis.db.api as api
 import louis.db as db
-from microbench import MicroBench
+from microbench import MicroBench, MBReturnValue, MBFunctionCall
 import pandas as pd
 
+class Bench(MicroBench, MBFunctionCall, MBReturnValue):
+    pass
 output_file = "benchmark_results.json"
 commit_version = subprocess.check_output(["git", "rev-parse", "HEAD"]).strip().decode('utf-8')
-basic_bench = MicroBench(outfile=output_file, commit_version=commit_version)
 
-# Execute the SQL search function 
+basic_bench = Bench(outfile=output_file, commit_version=commit_version)
+
 @basic_bench 
 def search(cursor, query):
+    """Execute the SQL search function"""
     return api.search_from_text_query(cursor, query)
 
 if __name__ == '__main__':
@@ -27,7 +30,5 @@ if __name__ == '__main__':
     with open(output_file, 'r') as result_file:
         print(pd.read_json(result_file, lines=True))
     
-    
-    # benchmark_results["MBReturnValue"] = MBReturnValue.__getattribute__
-    # benchmark_results["MBFunctionCall"] = MBFunctionCall.__getattribute__
+
 
