4d6c0a2cf4a8344c72fb7ff90135bc0d8304897d
melanie-fressard <Melanie.Fressard@inspection.gc.ca>
2023-10-31 18:58:11 +0000

    issue #20 - code review with jolan

--- search.py
+++ search.py
@@ -1,7 +1,3 @@
-# Name of this file: search.py
-# Location: at the root of the project
-# How to execute: python3 -m search.py query
-
 import subprocess
 import sys
 import louis.db.api as api
@@ -9,19 +5,22 @@ import louis.db as db
 from microbench import MicroBench, MBReturnValue, MBFunctionCall
 import pandas as pd
 
+
 class Bench(MicroBench, MBFunctionCall, MBReturnValue):
     pass
 
-output_file = "benchmarking/search_results.json"
-commit_version = subprocess.check_output(["git", 
-                                          "rev-parse", "HEAD"]).strip().decode('utf-8')
-basic_bench = Bench(outfile=output_file, commit_version=commit_version,
-                    function_version="0.0.1")
+
+outfile = "benchmarking/search_results.json"
+commit = subprocess.check_output(["git", "rev-parse", "HEAD"]).strip().decode("utf-8")
+basic_bench = Bench(
+    output_file=outfile, commit_version=commit, function_version="0.0.1"
+)
 
 
 def search(cursor, query):
     """Execute the SQL search function"""
     return api.search_from_text_query(cursor, query)
+
 
 @basic_bench
 def init_bench(query):
@@ -30,10 +29,9 @@ def init_bench(query):
     with db.cursor(connection) as cursor:
         return search(cursor, query)
 
-if __name__ == '__main__':
-    init_bench(' '.join(sys.argv[1:]))    
-    with open(output_file, 'r') as result_file:
+
+if __name__ == "__main__":
+    """How to execute: python3 -m search.py query"""
+    init_bench(" ".join(sys.argv[1:]))
+    with open(outfile, "r") as result_file:
         print(pd.read_json(result_file, lines=True))
-    
-
-
