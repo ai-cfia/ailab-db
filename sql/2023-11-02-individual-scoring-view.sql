da2ad526743499756fbb28625c24a29bc11af912
Mélanie F <73828657+melanie-fressard@users.noreply.github.com>
2023-11-09 19:18:35 +0000

    issue #41 - adding avg score

--- sql/2023-11-02-individual-scoring-view.sql
+++ sql/2023-11-02-individual-scoring-view.sql
@@ -1,8 +1,11 @@
-CREATE VIEW individual_scoring AS
+CREATE VIEW individual_score AS
 SELECT entity_id,
        CASE WHEN score_type = 'current' THEN score END AS current,
        CASE WHEN score_type = 'recency' THEN score END AS recency,
        CASE WHEN score_type = 'traffic' THEN score END AS traffic,
        CASE WHEN score_type = 'typicality' THEN score END AS typicality,
-       CASE WHEN score_type = 'similarity' THEN score END AS similarity
+       CASE WHEN score_type = 'similarity' THEN score END AS similarity,
+       COALESCE(current, 0) + COALESCE(recency, 0) +
+       COALESCE(traffic, 0) + COALESCE(typicality, 0) +
+       COALESCE(similarity, 0) AS avg
 FROM score;
