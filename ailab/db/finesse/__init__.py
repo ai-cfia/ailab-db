67eca6e542e443fa0509cade225867f6ea993d86
Mélanie F <73828657+melanie-fressard@users.noreply.github.com>
2023-11-15 21:29:25 +0000

    issue #41 - debbug connexion db

--- ailab/db/finesse/__init__.py
+++ ailab/db/finesse/__init__.py
@@ -4,6 +4,9 @@ dotenv.load_dotenv()
 
 def get_individual_scoring(cursor):
     """Creating a view with each individual scoring of documents"""
+    with open('sql/2023-11-02-individual-scoring-view.sql', 'r') as file:
+        sql_command = file.read()
+    cursor.execute(sql_command)
     cursor.execute("SELECT * FROM individual_score")
     return cursor.fetchall()
 
