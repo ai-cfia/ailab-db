403f782f46404d682767641541b6ebe87ffe8b7f
Mélanie F <73828657+melanie-fressard@users.noreply.github.com>
2023-11-16 16:48:35 +0000

    issue #41 - separating creation and select

--- ailab/db/finesse/__init__.py
+++ ailab/db/finesse/__init__.py
@@ -1,14 +1,5 @@
-import dotenv
-
-# This is used to load the .env file
-dotenv.load_dotenv()
-
-
 def get_individual_scoring(cursor):
-    """Creating a view with each individual scoring of documents by first reading the
-    SQL file and then executing a SELECT on it."""
-    with open("sql/2023-11-02-individual-scoring-view.sql", "r") as file:
-        sql_command = file.read()
-    cursor.execute(sql_command)
+    """Returning each individual scoring of documents by doing 
+    a SELECT on the individual_score view. Require louis_v006."""
     cursor.execute("SELECT * FROM individual_score")
     return cursor.fetchall()
--- bin/get-individual-scoring-finesse.py
+++ bin/get-individual-scoring-finesse.py
@@ -1,13 +1,27 @@
 import logging
 import ailab.db.finesse as finesse
 import ailab.db as db
+import pandas as pd
+import dotenv
 
+# This is used to load the .env file
+dotenv.load_dotenv()
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
+def create_individual_scoring(cursor):
+    """Create individual scoring view."""
+    with open("sql/2023-11-02-individual-scoring-view.sql", "r") as file:
+        sql_command = file.read()
+    cursor.execute(sql_command)
+
 if __name__ == "__main__":
-    """Get the individual scoring of each documents."""
+    """Get the individual scoring of each documents and write the results in a csv file."""
     connection = db.connect_db()
     with db.cursor(connection) as cursor:
-        finesse.get_individual_scoring(cursor)
+        create_individual_scoring(cursor)
+        res = finesse.get_individual_scoring(cursor)
+    with open('tests/output/individual_scoring.csv', 'w', newline='') as file:
+        df = pd.DataFrame(res)
+        df.to_csv('mon_fichier.csv', index=False)
