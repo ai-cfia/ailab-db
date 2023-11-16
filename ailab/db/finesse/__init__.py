21c66144aac5152316bdd8c898ddc794d42fe6db
Mélanie F <73828657+melanie-fressard@users.noreply.github.com>
2023-11-16 14:52:28 +0000

    issue #41 - formatting

--- ailab/db/finesse/__init__.py
+++ ailab/db/finesse/__init__.py
@@ -1,12 +1,14 @@
 import dotenv
+
 # This is used to load the .env file
 dotenv.load_dotenv()
 
+
 def get_individual_scoring(cursor):
-    """Creating a view with each individual scoring of documents"""
-    with open('sql/2023-11-02-individual-scoring-view.sql', 'r') as file:
+    """Creating a view with each individual scoring of documents by first reading the
+    SQL file and then executing a SELECT on it."""
+    with open("sql/2023-11-02-individual-scoring-view.sql", "r") as file:
         sql_command = file.read()
     cursor.execute(sql_command)
     cursor.execute("SELECT * FROM individual_score")
     return cursor.fetchall()
-
--- bin/get-individual-scoring-finesse.py
+++ bin/get-individual-scoring-finesse.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == "__main__":
-    """Get the individual scoring of each documents"""
+    """Get the individual scoring of each documents."""
     connection = db.connect_db()
     with db.cursor(connection) as cursor:
         finesse.get_individual_scoring(cursor)
