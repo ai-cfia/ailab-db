5733ba07f3104aa516205169cd432959e94f04fc
Mélanie F <73828657+melanie-fressard@users.noreply.github.com>
2023-11-29 19:39:41 +0000

    log on schema

--- tests/testing_utils.py
+++ tests/testing_utils.py
@@ -1,10 +1,14 @@
 import os
+import logging
 import dotenv
 dotenv.load_dotenv()
 
 def raise_error(message):
     raise Exception(message)
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+logger.info("Connecting to " + os.getenv("LOUIS_SCHEMA"))
 LOUIS_SCHEMA = os.getenv("LOUIS_SCHEMA") or raise_error("LOUIS_SCHEMA is not set")
 LOUIS_DSN = os.getenv("LOUIS_DSN") or raise_error("LOUIS_DSN is not set")
 MATCH_THRESHOLD = 0.5
