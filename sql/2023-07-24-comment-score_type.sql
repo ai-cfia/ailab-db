c8d5b5135336adf428e753b1ef19588576685259
Mélanie F <73828657+melanie-fressard@users.noreply.github.com>
2023-12-05 19:13:33 +0000

    issue #61 - link to file

--- sql/2023-07-24-comment-score_type.sql
+++ sql/2023-07-24-comment-score_type.sql
@@ -1,5 +1,5 @@
 comment on type score_type is 
 $COMMENT$
 score_type defines type of measurements that are taken into account.
-For more details about each scores, please read search-scores-and-weights.md
+For more details about each scores, please read 'https://github.com/ai-cfia/ailab-db/blob/main/search-scores-and-weights.md'
 $COMMENT$;
