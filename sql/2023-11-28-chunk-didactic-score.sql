5e716ce83a204049cda765b6f6fb9e847e14ded5
Jolan Thomassin <98430140+JolanThomassin@users.noreply.github.com>
2023-12-12 19:20:51 +0000

    Fixes #9, new SQL scripts

--- sql/2023-11-28-chunk-didactic-score.sql
+++ sql/2023-11-28-chunk-didactic-score.sql
@@ -1,5 +1,6 @@
 -- Set the search path to the louis_006 schema
-SET search_path TO louis_006;
+SET
+    search_path TO "louis_0.0.6";
 
 CREATE TABLE IF NOT EXISTS chunk_score (
     id UUID,
@@ -9,28 +10,36 @@ CREATE TABLE IF NOT EXISTS chunk_score (
 
 TRUNCATE TABLE chunk_score;
 
-INSERT INTO chunk_score (id, score, score_type)
+INSERT INTO
+    chunk_score (id, score, score_type)
 SELECT
-    ch.id, -- Use the id column from the chunk table
+    ch.id,
+    -- Use the id column from the chunk table  
     ROUND(
-        (
-            LENGTH(hc.content) - length_values.min_val
+        1 - (
+            (
+                LENGTH(hc.content) - LENGTH(REPLACE(hc.content, '<tr>', ''))
+            ) / LENGTH('<tr>') - length_values.min_val
         ) * 1.0 / (length_values.max_val - length_values.min_val),
         1
     ) AS tr_proportion,
     'didactic' AS score_type
 FROM
-    louis_006.chunk ch
-INNER JOIN louis_006.html_content_to_chunk hctc ON ch.id = hctc.chunk_id
-INNER JOIN louis_006.html_content hc ON hctc.md5hash = hc.md5hash
-CROSS JOIN (
-    SELECT
-        MIN(LENGTH(content)) AS min_val,
-        MAX(LENGTH(content)) AS max_val
-    FROM
-        louis_006.chunk ch
-    INNER JOIN louis_006.html_content_to_chunk hctc ON ch.id = hctc.chunk_id
-    INNER JOIN louis_006.html_content hc ON hctc.md5hash = hc.md5hash
-) AS length_values
+    "louis_0.0.6".chunk ch
+    INNER JOIN "louis_0.0.6".html_content_to_chunk hctc ON ch.id = hctc.chunk_id
+    INNER JOIN "louis_0.0.6".html_content hc ON hctc.md5hash = hc.md5hash
+    CROSS JOIN (
+        SELECT
+            MIN(
+                LENGTH(content) - LENGTH(REPLACE(content, '<tr>', ''))
+            ) / LENGTH('<tr>') AS min_val,
+            MAX(
+                LENGTH(content) - LENGTH(REPLACE(content, '<tr>', ''))
+            ) / LENGTH('<tr>') AS max_val
+        FROM
+            "louis_0.0.6".chunk ch
+            INNER JOIN "louis_0.0.6".html_content_to_chunk hctc ON ch.id = hctc.chunk_id
+            INNER JOIN "louis_0.0.6".html_content hc ON hctc.md5hash = hc.md5hash
+    ) AS length_values
 ORDER BY
     tr_proportion DESC;
--- sql/2023-12-11-score-by-element.sql
+++ sql/2023-12-11-score-by-element.sql
@@ -1,9 +1,10 @@
 -- Set the search path to the louis_006 schema
-SET search_path TO "louis_0.0.6";
+SET
+    search_path TO "louis_0.0.6";
     
 WITH tr_counts AS (
     SELECT
-        c.id,
+        id,
         (LENGTH(h.content) - LENGTH(REPLACE(h.content, '<tr>', ''))) / LENGTH('<tr>') AS tr_count
     FROM
         crawl c
@@ -48,3 +49,38 @@ histogram AS (
     ORDER BY buckets.bucket_min, buckets.bucket_max
 )
 SELECT bucket_min, bucket_max, freq, bar FROM histogram;
+
+
+-- Set the search path to the louis_006 schema
+SET
+    search_path TO "louis_0.0.6";
+    
+WITH tr_counts AS (
+    SELECT
+        id,
+        (LENGTH(h.content) - LENGTH(REPLACE(h.content, '<tr>', ''))) / LENGTH('<tr>') AS tr_count
+    FROM
+        crawl c
+        INNER JOIN html_content h ON c.md5hash = h.md5hash
+),
+tr_stats AS (
+    SELECT
+        id,
+        tr_count,
+        MAX(tr_count) OVER () AS max_tr_count,
+        MIN(tr_count) OVER () AS min_tr_count
+    FROM
+        tr_counts
+)
+SELECT
+    id,
+    tr_count,
+    -- Normalize tr_count into a score between 1 and 0, with higher tr_count getting lower score
+    CASE
+        WHEN max_tr_count = min_tr_count THEN 0.0 -- Avoid division by zero, assign lowest score
+        ELSE 1.0 - ((tr_count - min_tr_count)::FLOAT / (max_tr_count - min_tr_count))
+    END AS score
+FROM
+    tr_stats
+ORDER BY
+    score ASC;
