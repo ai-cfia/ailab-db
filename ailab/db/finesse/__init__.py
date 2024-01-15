4f8664d88208f0704223fd6847dbc071c9d0da11
JolanThomassin <jolan.thomassin@inspection.gc.ca>
2024-01-15 15:31:20 +0000

    Fixes #9, magic string

--- ailab/db/finesse/__init__.py
+++ ailab/db/finesse/__init__.py
@@ -1,6 +1,9 @@
 import os
 import json
 
+# Constants
+DEFAULT_JSON_PATH = "/ailab/db/finesse/prompt"
+
 
 def load_prompt(prompt_path, filename):
     full_path = os.path.join(prompt_path, filename)
@@ -11,7 +14,13 @@ def load_prompt(prompt_path, filename):
     return content
 
 
-def load_json_template(json_path):
+def load_json_template(json_path=DEFAULT_JSON_PATH):
+    """
+    Load a JSON template from the specified path.
+
+    Returns:
+    str: A JSON-formatted string representing the loaded data.
+    """
     json_file_path = json_path + "/json_template.json"
 
     with open(json_file_path, "r") as file:
